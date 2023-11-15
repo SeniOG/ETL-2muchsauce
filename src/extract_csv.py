36a6e44949db562e75f8929912db1b6251770ccd
Seni Ogunmilade <seni@hotmail.co.uk>
2023-11-15 11:44:49 +0000

    test2

--- src/extract_csv.py
+++ src/extract_csv.py
@@ -10,3 +10,4 @@ test_file = read_csv('test_file.csv')
 print(test_file)           #To check if it reads correctly
 print(len(test_file))      #To check each line in the csv file is read into 1 item in the list
 
+# test 2
