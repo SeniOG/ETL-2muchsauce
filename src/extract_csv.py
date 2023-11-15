168ed55920a3be9278fdba7242b00f12fa5595ef
Seni Ogunmilade <seni@hotmail.co.uk>
2023-11-15 10:57:30 +0000

    testing branches

--- src/extract_csv.py
+++ src/extract_csv.py
@@ -9,3 +9,5 @@ test_file = read_csv('test_file.csv')
 
 print(test_file)           #To check if it reads correctly
 print(len(test_file))      #To check each line in the csv file is read into 1 item in the list
+
+# test text
