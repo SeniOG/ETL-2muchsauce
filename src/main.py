7ddf6e748074aa44f8d6b558eb7bb3fe29cd2044
Seni Ogunmilade <seni@hotmail.co.uk>
2023-11-22 21:01:37 +0000

    created a main file for running all other functions

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from check_floats import convert_floats_cols
+from extract_csv import read_file
+from format_date import format_date
+from remove_duplicate import remove duplicate
+from remove_empty_value import clean_empty data
+from remove_sens_data import extraction
+from seperate_item_price import item_price
+
+# set file names
+
+# extract data
+
+# transform data
+
+# load data
