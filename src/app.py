09346cd0d260ce7d7cf96ca38551a84078f5c316
Seni Ogunmilade <seni@hotmail.co.uk>
2023-11-23 12:18:54 +0000

    corrected file names and syntax in app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
 from check_floats import convert_floats_cols
 from extract_csv import read_file
 from format_date import format_date
-from remove_duplicate import remove duplicate
-from remove_empty_value import clean_empty data
+from remove_duplicate import remove_duplicate
+from remove_empty_value import clean_empty_data
 from remove_sens_data import extraction
 from seperate_item_price import item_price
 
 # set file names
+raw_data = read_file('test_file.csv')
+
+print('app.py test')
+print(raw_data)
 
 # extract data
 
