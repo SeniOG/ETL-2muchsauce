59fbe757976dc17873899244dce837c7bccecea7
Seni Ogunmilade <seni@hotmail.co.uk>
2023-11-27 15:05:13 +0000

    Added AWS functionality

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 from check_floats import convert_floats_cols
-from extract_csv import read_file
 from format_date import format_date
 from remove_duplicate import remove_duplicate
 from remove_empty_value import clean_empty_data
@@ -7,7 +6,7 @@ from remove_sens_data import remove_sens
 from seperate_item_price import item_price
 
 # set file names and extract data
-raw_data = read_file('test_file.csv')
+# raw_data = read_file('test_file.csv')
 
 # test code
 # print('\napp.py test\n')
@@ -16,7 +15,7 @@ raw_data = read_file('test_file.csv')
 
 
 # transform data
-def transform():
+def transform(raw_data):
     step_one = clean_empty_data(raw_data)
     step_two= remove_duplicate(step_one)
     step_three = remove_sens(step_two)
@@ -27,13 +26,13 @@ def transform():
     final_step = step_six
     return final_step
 
-print('\n >>>>>TRANSFORMED DATA<<<<<<<<<<')
-transformed_data = transform()
+# # test code
+# print('\n >>>>>TRANSFORMED DATA<<<<<<<<<<')
+# transformed_data = transform()
 
-# print(transformed_data)
-print('\n')
-for line in transformed_data:
-    print(f'{line}\n')
+# print('\n')
+# for line in transformed_data:
+#     print(f'{line}\n')
 
 
 # load data
