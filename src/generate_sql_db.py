adad74ff20d3d8cb5c4ca50b0398275ef83369d6
Seni Ogunmilade <seni@hotmail.co.uk>
2023-12-09 23:45:03 +0000

    Branch created to match with latest version of AWS Lambda function. Up to date as of Dec 9th 2023

---
+++ src/generate_sql_db.py
@@ -0,0 +1,60 @@
+from connect_to_db import *
+
+def create_db_tables(connection, cursor) -> bool:
+    print('create_db_tables started')
+    try:
+        #Drop tables if they exist
+        drop_tables_sql = (
+            "DROP TABLE IF EXISTS Order_breakdown;",
+            "DROP TABLE IF EXISTS Orders ;",
+            "DROP TABLE IF EXISTS products;"
+        )
+        
+        for command in drop_tables_sql:
+            cursor.execute(command)
+        
+        # # Create sequences
+        # cursor.execute("CREATE SEQUENCE products_productid_seq;")
+        # cursor.execute("CREATE SEQUENCE payments_typeid_seq;")
+        # cursor.execute("CREATE SEQUENCE branch_branchid_seq;")
+        # cursor.execute("CREATE SEQUENCE transactions_orderid_seq;")
+        
+        # Create tables
+        create_tables_sql = ("""
+            CREATE TABLE IF NOT EXISTS Orders (
+                Order_id VARCHAR(36) PRIMARY KEY,
+                order_date TIMESTAMP,
+                Payment_type VARCHAR(255),
+                Branch VARCHAR(255)
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS Products (
+                Product_id VARCHAR(36) PRIMARY KEY,
+                product_name VARCHAR(255) NOT NULL,
+                Price DECIMAL(10,2) NOT NULL -- Changing data type to NUMERIC
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS Order_breakdown (
+                Order_ID VARCHAR(36),
+                Product_ID VARCHAR(36),
+                Quantity NUMERIC,
+                Total_price DECIMAL(10,2),
+                PRIMARY KEY (Order_ID, Product_ID),
+                FOREIGN KEY (Order_ID) REFERENCES Orders(Order_id),
+                FOREIGN KEY (Product_ID) REFERENCES Products(Product_id)
+            );
+            """
+            )
+        # note to self: i dont like this basket table. it has as a key, the unique pairing of orderid and productid
+        for command in create_tables_sql:
+            cursor.execute(command)
+
+        connection.commit()
+        print('...committed')
+        print('create_db_tables done')
+        return True
+    except Exception as ex:
+        print(f'create_db_tables failed to generate table/s:\n{ex}')
+        return False
